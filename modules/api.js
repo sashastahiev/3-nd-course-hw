87ceb5562f54fb31203cb3640c99f65dc56238ec
sashastahiev <vebium@mail.ru>
2025-05-29 14:33:26 +0300

    Create hw7 from 29.05.2025

--- modules/api.js
+++ modules/api.js
@@ -14,12 +14,31 @@ export const fetchPostComments = (newComment2, message) => {
     return fetch('https://wedev-api.sky.pro/api/v1/:stahiev-aleks/comments', {
     method: 'POST', 
     body: JSON.stringify(newComment2)
-    }).then(() => {
+    }).then((response) => {
+        if (response.status == 201){
+            return response.json()
+        }
+        else {
+            if (response.status == 500){
+                throw new Error('Сервер упал')
+            }
+            if (response.status == 400){
+                throw new Error('Вы допустили ошибку')
+            }
+            throw new Error('Что-то пошло не так')
+        }
+    })
+    .then(() => {
         return fetchGetComments()
     }).then(() => {
         addForm.style.display = 'block'
         message.remove()
         textcommentEl.value = ''
         nameEl.value = ''
+    }).catch((error) => {
+        alert(error)
+    }).finally(() => {
+        addForm.style.display = 'block'
+        message.remove()
     })
 }
